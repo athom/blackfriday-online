a6d96f59b14018391781d767695ca1c56391989f
athom <athom@126.com>
2013-09-12 09:21:41 +0800

    change repo again and remove discarded flags

--- handlers/markdown.go
+++ handlers/markdown.go
@@ -1,7 +1,7 @@
 package handlers
 
 import (
-	"github.com/russross/blackfriday"
+	"github.com/athom/blackfriday"
 )
 
 var (
@@ -11,7 +11,7 @@ var (
 
 func initMarkdownFlags() {
 	htmlFlags := 0
-	htmlFlags |= blackfriday.HTML_ESCAPE_HTML
+	//htmlFlags |= blackfriday.HTML_ESCAPE_HTML
 	htmlFlags |= blackfriday.HTML_USE_XHTML
 	htmlFlags |= blackfriday.HTML_USE_SMARTYPANTS
 	htmlFlags |= blackfriday.HTML_SMARTYPANTS_LATEX_DASHES
@@ -25,11 +25,9 @@ func initMarkdownFlags() {
 	extensions |= blackfriday.EXTENSION_FENCED_CODE
 	extensions |= blackfriday.EXTENSION_AUTOLINK
 	extensions |= blackfriday.EXTENSION_STRIKETHROUGH
-	extensions |= blackfriday.EXTENSION_NO_SPACE_LISTS
 	extensions |= blackfriday.EXTENSION_HARD_LINE_BREAK
 	extensions |= blackfriday.EXTENSION_NO_EMPTY_LINE_BEFORE_BLOCK
 	extensions |= blackfriday.EXTENSION_ONE_SPACE_INDENT
-	extensions |= blackfriday.EXTENSION_UNICODE_LIST_ITEM
 	mdExtensions = extensions
 }
 
